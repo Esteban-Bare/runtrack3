e9de2d5a70185a956653198cb95251c84f4e66d2
Esteban <esteban5.bare@gmail.com>
2024-02-12 11:40:30 +0100

    Job-01 Done

---
+++ jour01/job-01/index.php
@@ -0,0 +1,15 @@
+ <!DOCTYPE html>
+ <html lang="en">
+ <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <script> function myPopUp() {
+        alert("Hello js");
+    }</script>
+ </head>
+ <body>
+ <p>davkdjkazhvdazjdvazdjazvk</p>
+    <button onclick='myPopUp()' id='x'>Click here</button>
+ </body>
+ </html>             
