5866a898da3c85e471a621c0f4f211caaa3bb806
Esteban <esteban5.bare@gmail.com>
2024-02-13 15:33:37 +0100

    j2-job-01 done

---
+++ jour02/job-01/1.js
@@ -0,0 +1,5 @@
+const article = document.getElementById("citation");
+
+function citation() { 
+    console.log(article.textContent); 
+}
---
+++ jour02/job-01/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <article id="citation">La vie a beaucoup plus d'imagination que nous</article>
+    <button onclick="citation()" id="button">aa</button>
+    <script src="1.js">
+    </script>
+</body>
+
+</html>
